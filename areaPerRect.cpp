c3b8485ebc76dcafd068a243adc5bcdc886b919b
Amara Tie <amara.tie@stu.ocsb.ca>
2025-02-19 17:46:21 +0000

    areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Amara Tie All rights reserved.
+
+//
+
+// Created by: Amara Tie
+
+// Date: Febuary 19, 2025
+
+// This program demonstrates how to calculate the area and perimeter in C++
+//  with the dimensions of 2cm x 7cm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If the rectangle has the dimensions" << std::endl;
+    std::cout << "2cm x 7cm"  << std::endl;
+    std::cout << ""  << std::endl;
+    std::cout << "The Area is:" << (2*7) << "cm^2" << std::endl;
+    std::cout << "The Perimeter is:" << (2*(5+3)) << "cm" << std::endl;
+
+    return 0;
+}
